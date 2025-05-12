f6683263a5a7f70bc409105bd1defa12782a87ac
AlexTereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2025-05-12 21:49:10 +0200

    Add project files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Build
+
+```shell
+rm -rf build/
+mkdir build/
+cd build/
+cmake ..
+make
+./simple_executable
+```
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Invoking main.cpp from cmake-sample-executable project!" << std::endl;
+    return 0;
+}
